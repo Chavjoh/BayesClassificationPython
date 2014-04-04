4b418cee7bcf1f2d47674a94c5070f40771f54f5
ice-blaze <franketip@gmail.com>
2014-04-04 10:13:56 +0200

    Add DataFile class to split words of a line and count it

--- BayesClassification.py
+++ BayesClassification.py
@@ -27,6 +27,41 @@ import sys
 #------------------------------------------------------------------------------#
 
 
+class DataFile:
+
+    def __init__(self, fileLine, isGood):
+        """
+
+        :rtype : object
+        """
+        self.isGood = isGood
+        self.fileLine = fileLine
+        self.wordsCount = {}
+
+        self.words = fileLine.split()
+
+        for word in self.words:
+            try:
+                self.wordsCount[word] += 1
+            except KeyError:
+                self.wordsCount[word] = 1
+
+        self.sumWords = sum(self.wordsCount.values())
+
+    def __repr__(self):
+        print("input : "+self.fileLine)
+        for key, val in self.wordsCount.items():
+            print(str(key)+" "+str(val))
+
+        print(str(self.sumWords))
+        return ""
+
+
+
+
+
+
+
 #------------------------------------------------------------------------------#
 #                                                                              #
 #                             UTILITIES FUNCTIONS                              #
@@ -45,3 +80,6 @@ if __name__ == '__main__':
 
     argsCount = len(sys.argv)
     argsIndex = 1
+
+    toto = DataFile("coucou je suis une grosse bite et je vous emmerde Monsieur le PD n'ha n'ha n'aire", True)
+    print(toto)
