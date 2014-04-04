e10874d31551bda0e8754e4c3fe1182c733cbed0
Chavjoh <j.chavaillaz@gmail.com>
2014-04-04 13:11:51 +0200

    Manage tagged data directories.

--- BayesClassification.py
+++ BayesClassification.py
@@ -32,20 +32,30 @@ import random
 class DataFile:
     """ Contains file analysis information """
 
-    def __init__(self, fileLine, isPositive):
+    def __init__(self, **keys):
         """
         Create a new DataFile
-        :param fileLine: Data file content (=message) in one line
-        :type fileLine: str
+        :param fileContent: Data file content (=message)
+        :type fileContent: str
         :param isPositive: True if positive message, False otherwise
         :type isPositive: bool
         :rtype: str
         """
-        self.isPositive = isPositive
-        self.fileLine = fileLine
+        self.isPositive = keys['isPositive']
+        self.fileContent = keys['fileContent']
+        self.words = []
         self.wordsCount = {}
 
-        self.words = fileLine.split()
+        if keys['isTagged']:
+            self.loadTagged()
+        else:
+            self.load()
+
+        self.sumWords = sum(self.wordsCount.values())
+
+    def load(self):
+
+        self.words = self.fileContent.split()
 
         for word in self.words:
             try:
@@ -53,10 +63,12 @@ class DataFile:
             except KeyError:
                 self.wordsCount[word] = 1
 
-        self.sumWords = sum(self.wordsCount.values())
+    def loadTagged(self):
+        # TODO
+        pass
 
     def __repr__(self):
-        information = "Input file : " + self.fileLine + "\n"
+        information = "Input file : " + self.fileContent + "\n"
         information += "============" + "\n"
 
         for key, value in self.wordsCount.items():
@@ -70,14 +82,18 @@ class DataFile:
 class DataSet:
     """ Contains all DataFile """
 
-    def __init__(self, dataSetPath):
+    def __init__(self, dataSetPath, isTagged):
         """
         :param dataSetPath: Path to data set folder that contains positive and negative folder messages
         :type dataSetPath: str
+        :param isTagged: Indicate if dataSet is tagged (Different file structure)
+        :type isTagged: bool
         :return: object
         """
+        self.testSuccess = 0
         self.dataPositive = []
         self.dataNegative = []
+        self.isTagged = isTagged
         self.dataPath = dataSetPath
         self.positivePath = self.dataPath + "/positive"
         self.negativePath = self.dataPath + "/negative"
@@ -103,8 +119,18 @@ class DataSet:
                     # print(directoryPath + '/' + fileName)
                     fileContent = ''.join(open(directoryPath + '/' + fileName, 'r', encoding="utf-8").readlines())
 
-                    self.dataPositive.append(DataFile(fileContent, isPositive)) if isPositive \
-                        else self.dataNegative.append(DataFile(fileContent, isPositive))
+                    if isPositive:
+                        self.dataPositive.append(DataFile(
+                            fileContent=fileContent,
+                            isPositive=isPositive,
+                            isTagged=self.isTagged
+                        ))
+                    else:
+                        self.dataNegative.append(DataFile(
+                            fileContent=fileContent,
+                            isPositive=isPositive,
+                            isTagged=self.isTagged
+                        ))
 
             for subDirectory in subDirectoryList:
                 self.load(path + "/" + subDirectory)
@@ -117,34 +143,47 @@ class DataSet:
 
         :return: null
         """
-        maxIndexPositive = int(0.8 * self.dataPositive.count())
-        maxIndexNegative = int(0.8 * self.dataNegative.count())
+        maxIndexPositive = int(0.8 * len(self.dataPositive))
+        maxIndexNegative = int(0.8 * len(self.dataNegative))
 
         for data in (self.dataPositive[:maxIndexPositive] + self.dataNegative[:maxIndexNegative]):
-
+            # TODO
             pass
 
     def test(self):
         """
         Testing results from bayes algorithm
+        Save number of correct values
 
         :return: null
         """
-        maxIndexPositive = int(0.8 * self.dataPositive.count())
-        maxIndexNegative = int(0.8 * self.dataNegative.count())
+        maxIndexPositive = int(0.8 * len(self.dataPositive))
+        maxIndexNegative = int(0.8 * len(self.dataNegative))
 
         for data in (self.dataPositive[maxIndexPositive:] + self.dataNegative[maxIndexNegative:]):
-
+            # TODO
+            # Update self.testSuccess
             pass
+
+    def evaluate(self):
+        """
+        Accuracy calculation to evaluate training algorithm
+        :return: float
+        """
+        accuracy = self.testSuccess / (len(self.dataPositive) + len(self.negativePath))
+
+        return accuracy
 
     def classify(self, dataFile):
         """
         Classify a dataFile to positive or negative
 
         :param dataFile: DataFile to classify
+        :type dataFile: DataFile
         :return: True if positive, False otherwise
         """
 
+        # TODO
         pass
 
 #------------------------------------------------------------------------------#
@@ -167,5 +206,14 @@ if __name__ == '__main__':
 
     random.seed()
 
-    dataSet = DataSet("./data")
-    print(dataSet.dataPositive[500])
+    dataSet = DataSet("./data", False)
+    # print(dataSet.dataPositive[500])
+    dataSet.train()
+    dataSet.test()
+    print(dataSet.evaluate())
+
+    dataSetTagged = DataSet("./data/tagged", True)
+    # print(dataSet.dataPositive[500])
+    dataSetTagged.train()
+    dataSetTagged.test()
+    print(dataSetTagged.evaluate())
